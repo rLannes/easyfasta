36cd3ddc181a6e306f0105e8d577f43fb389532b
rLannes <romain.lannes@gmail.com>
2025-08-07 20:54:41 -0400

    pre

--- easyfasta/__init__.py
+++ easyfasta/__init__.py
@@ -1,4 +1,27 @@
-from .easyfasta import *
+from .easyfata import(
+    wrap_sequence,
+    build_index,
+    get_sequence_index,
+    get_sequence_id,
+    fasta_iter,
+    load_fasta,
+    complement,
+    reverse,
+    reverse_complement
+)
+
+__all__ = [
+    "wrap_sequence",
+    "build_index",
+    "get_sequence_index",
+    "get_sequence_id",
+    "fasta_iter",
+    "load_fasta",
+    "complement",
+    "reverse",
+    "reverse_complement"
+
+]
 
 __version__ = "1.0.4"
 
